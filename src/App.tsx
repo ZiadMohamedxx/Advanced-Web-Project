a160e9746c9615b10b98b22a28c63d7a04048553
Eyadyasser-7 <eyadyassergamal7@gmail.com>
2026-04-25 02:07:50 +0300

    Refactor routing structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
 import CandidatePortal from "./pages/CandidatePortal";
@@ -21,7 +22,6 @@ import EmployerDashboard from "./pages/Employerdashboard";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 
-
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -30,55 +30,33 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
+
         <BrowserRouter>
           <Routes>
-          <Route element={<Layout />}>
-    <Route path="/" element={<Index />} />
-    <Route path="/candidate-portal" element={<CandidatePortal />} />
-    <Route path="/employer-portal" element={<EmployerPortal />} />
-    <Route path="/jobs" element={<Jobs />} />
-    <Route path="/accessibility" element={<AccessibilityPage />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/signup" element={<SignUp />} />
-    <Route path="/signin" element={<SignIn />} />
-    <Route path="/profile" element={<Profile />} />
-    <Route path="/employer-dashboard" element={<EmployerDashboard />} />
-    <Route path="/apply/:jobId" element={<ApplyJob />} />
-    <Route path="/post-job" element={<PostJob />} />
+            <Route element={<Layout />}>
+              <Route path="/" element={<Index />} />
+              <Route path="/candidate-portal" element={<CandidatePortal />} />
+              <Route path="/employer-portal" element={<EmployerPortal />} />
+              <Route path="/jobs" element={<Jobs />} />
+              <Route path="/accessibility" element={<AccessibilityPage />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+              <Route path="/apply/:jobId" element={<ApplyJob />} />
+              <Route path="/post-job" element={<PostJob />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password/:token" element={<ResetPassword />} />
             </Route>
+
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+
       </TooltipProvider>
     </QueryClientProvider>
   </LanguageProvider>
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-        <Route element={<Layout />}>
-  <Route path="/" element={<Index />} />
-  <Route path="/candidate-portal" element={<CandidatePortal />} />
-  <Route path="/employer-portal" element={<EmployerPortal />} />
-  <Route path="/jobs" element={<Jobs />} />
-  <Route path="/accessibility" element={<AccessibilityPage />} />
-  <Route path="/about" element={<About />} />
-  <Route path="/signup" element={<SignUp />} />
-  <Route path="/signin" element={<SignIn />} />
-  <Route path="/profile" element={<Profile />} />
-  <Route path="/employer-dashboard" element={<EmployerDashboard />} />
-  <Route path="/apply/:jobId" element={<ApplyJob />} />
-  <Route path="/post-job" element={<PostJob />} />
-  <Route path="/forgot-password" element={<ForgotPassword />} />
-  <Route path="/reset-password/:token" element={<ResetPassword />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
 );
 
 export default App;
